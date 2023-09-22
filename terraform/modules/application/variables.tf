ef04da6b48c94901de40a108a8ff1858ab43ed3a
Renne Rocha <renne@rennerocha.com>
2023-09-22 16:06:58 -0300

    New version

---
+++ terraform/modules/application/variables.tf
@@ -0,0 +1,37 @@
+variable "application" {
+  default = "the-project"
+}
+
+variable "environment" {
+  default = "sandbox"
+}
+
+variable "domain_zone" {
+  default = "sixfeetup.com"
+}
+
+variable "api_domain" {
+  type    = string
+  default = "api.sixfeetup.com"
+}
+
+variable "cluster_domain" {
+  default = "k8s.sixfeetup.com"
+}
+
+variable "cluster_public_id" {
+  default = ""
+}
+
+variable "cluster_id" {
+  default = ""
+}
+
+variable "cluster_arn" {
+  default = ""
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
---
+++ terraform/modules/global_variables/main.tf
@@ -0,0 +1,26 @@
+output "app_name" {
+  description = "App Name"
+  value       = "the project"
+}
+
+output "application" {
+  value = "the-project"
+}
+
+output "aws_region" {
+  description = "AWS Region"
+  value       = "us-east-1"
+}
+
+output "aws_profile" {
+  description = "AWS Profile for CLI"
+  value       = "the-project"
+}
+
+output "account_id" {
+  value = "000000000000"
+}
+
+output "domain_name" {
+  value = "sixfeetup.com"
+}
