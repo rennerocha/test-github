e40331b3fd1ce24b523656206d11b192d52b6d85
Renne Rocha <renne@rennerocha.com>
2023-09-26 11:16:00 -0300

    Updated

---
+++ backend/my_awesome_sixie_project/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from my_awesome_sixie_project.users.models import User
+from my_awesome_sixie_project.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
